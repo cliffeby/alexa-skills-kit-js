52d25c45decf260a7e1765efa998941f318272f8
Cliff Eby <ebyc@pbworld.com>
2016-05-20 15:58:02 -0400

    PeePaw age branch commit

--- samples/peePaw/src/index.js
+++ samples/peePaw/src/index.js
@@ -1,12 +1,9 @@
 /**
- Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
- http://aws.amazon.com/apache2.0/
-
- or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+        http://aws.amazon.com/apache2.0/
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
 
 /**
  * This simple sample has no external dependencies or session management, and shows the most basic
@@ -34,7 +31,7 @@ var AlexaSkill = require('./AlexaSkill');
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var PeePaw = function () {
+    var PeePaw = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
@@ -66,6 +63,9 @@ PeePaw.prototype.intentHandlers = {
     "VioletNudieIntent": function (intent, session, response) {
         response.tell("When Violet calls, PeePaw says nudi nudi nudie!");
     },
+    "VioletAgeIntent": function (intent, session, response) {
+        getAgeinYearsMonthsDays(intent, session, response);
+    },
     "MollyIntent": function (intent, session, response) {
         response.tell("Molly can say taco");
     },
@@ -93,46 +93,24 @@ PeePaw.prototype.intentHandlers = {
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
         };
         response.ask(speechOutput, repromptOutput);
-    },
-    "VioletAgeIntent": function (intent, session, response) {
-        //getAgeinYearsMonthsDays(intent, session, response);
-        var today = new Date();
-        var thisday = today.getDate();
-        var bDay = new Date(2012, 8, 3);
-        var diff = today - bDay;
-        var day = 1000 * 60 * 60 * 24;
-        console.log(days, months, years);
-        var days = Math.floor(diff / day);
-        var months = Math.floor(days / 31);
-        var years = Math.floor(months / 12);
-        var speechOutput = "Peepaw thinks that Violet is  " + years + "years," + months + "months, and" + days + "days,old";
-        response.tell(speechOutput);
     }
 };
-//     "AMAZON.HelpIntent": function (intent, session, response) {
-//         response.ask("You can say hello to me!", "You can say hello to me!");
-//     }
-// };
+function getAgeinYearsMonthsDays(intent, session, response) {
+    var bDay = new Date('2012-08-03');
+    var difdt = new Date(new Date() - bDay);
+    console.log((difdt.toISOString().slice(0, 4) - 1970) + "Y " + (difdt.getMonth()+1) + "M " + difdt.getDate() + "D");
+    var years = difdt.toISOString().slice(0,4)-1970;
+    var months = difdt.getMonth()+1;
+    var days = difdt.getDate();
+    var speechOutput = "Peepaw thinks that Violet is  " + years + " years," + months + " months, and" + days + " days old";
+    console.log(speechOutput);
+    response.tell(speechOutput);
+};
 
-// function getAgeinYearsMonthsDays(intent, session, response) {
-//         var today = new Date();
-//     var thisday = today.getDate();
-//     var bDay = new Date(2012, 8, 3, 0, 0);
-//     var diff = thisday - bDay;
-//     var day = 1000 * 60 * 60 * 24;
-//     console.log(days, months, years);
-//     var days = Math.floor(diff / day);
-//     var months = Math.floor(days / 31);
-//     var years = Math.floor(months / 12);
-//     var speechOutput = "Peepaw thinks that Violet is  " + years + "years," + months + "months, and" + days + "days,old";
-//     response.tell(speechOutput);
-//     //     response.tell(speechOutput);
-//     };
 
 // Create the handler that responds to the Alexa Request.
-    exports.handler = function (event, context) {
-        // Create an instance of the PeePaw skill.
-        var helloWorld = new PeePaw();
-        helloWorld.execute(event, context);
-    };
-
+exports.handler = function (event, context) {
+    // Create an instance of the PeePaw skill.
+    var helloWorld = new PeePaw();
+    helloWorld.execute(event, context);
+};
