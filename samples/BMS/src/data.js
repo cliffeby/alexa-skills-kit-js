fe4da5b0c9575eb4aff632d1ee63f455813f5759
cliffeby <clifford.eby@gmail.com>
2016-05-21 16:00:57 -0400

    Added BMS

---
+++ samples/BMS/src/data.js
@@ -0,0 +1,382 @@
+/**
+ * Created by cliff on 5/21/2016.
+ */
+/**
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+ http://aws.amazon.com/apache2.0/
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+module.exports = {
+    "201": {
+        "Region": "NJ",
+        "TimezoneOffset": "-5",
+        "Description": "Northern New Jersey: Jersey City, Hackensack"
+    },
+    "202": {
+        "Region": "DC",
+        "TimezoneOffset": "-5",
+        "Description": "Washington, D.C."
+    },
+    "203": {
+        "Region": "CT",
+        "TimezoneOffset": "-5",
+        "Description": "Connecticut: Fairfield County and New Haven County; Bridgeport, New Haven"
+    },
+    "204": {
+        "Region": "MB",
+        "TimezoneOffset": "-6",
+        "Description": "Canada: Manitoba"
+    },
+    "205": {
+        "Region": "AL",
+        "TimezoneOffset": "-6",
+        "Description": "Central Alabama including Birmingham; excludes the southeastern corner of Alabama and the deep south"
+    },
+    "206": {
+        "Region": "WA",
+        "TimezoneOffset": "-8",
+        "Description": "Western Washington state: Seattle and Bainbridge Island"
+    },
+    "207": {
+        "Region": "ME",
+        "TimezoneOffset": "-5",
+        "Description": "Maine"
+    },
+    "208": {
+        "Region": "ID",
+        "TimezoneOffset": "0.875",
+        "Description": "Idaho"
+    },
+    "209": {
+        "Region": "CA",
+        "TimezoneOffset": "-8",
+        "Description": "Central California: Stockton"
+    },
+    "210": {
+        "Region": "TX",
+        "TimezoneOffset": "-6",
+        "Description": "Southern Texas: San Antonio"
+    },
+    "211": {
+        "Region": "None",
+        "TimezoneOffset": "None",
+        "Description": "Local community info / referral services"
+    },
+    "212": {
+        "Region": "NY",
+        "TimezoneOffset": "-5",
+        "Description": "New York City, New York including Manhattan"
+    },
+    "213": {
+        "Region": "CA",
+        "TimezoneOffset": "-8",
+        "Description": "Southern California: Los Angeles"
+    },
+    "214": {
+        "Region": "TX",
+        "TimezoneOffset": "-6",
+        "Description": "Texas: Dallas Metro"
+    },
+    "215": {
+        "Region": "PA",
+        "TimezoneOffset": "-5",
+        "Description": "South Eastern Pennsylvania: Philadelphia"
+    },
+    "216": {
+        "Region": "OH",
+        "TimezoneOffset": "-5",
+        "Description": "Ohio: Cleveland"
+    },
+    "217": {
+        "Region": "IL",
+        "TimezoneOffset": "-6",
+        "Description": "Central Illinois: Springfield"
+    },
+    "218": {
+        "Region": "MN",
+        "TimezoneOffset": "-6",
+        "Description": "Northern Minnesota: Duluth"
+    },
+    "219": {
+        "Region": "IN",
+        "TimezoneOffset": "1.2",
+        "Description": "North Western Indiana: Gary"
+    },
+    "220": {
+        "Region": "OH",
+        "TimezoneOffset": "-5",
+        "Description": "South Eastern and Central Ohio"
+    },
+    "224": {
+        "Region": "IL",
+        "TimezoneOffset": "-6",
+        "Description": "North Eastern Illinois: Evanston, Waukegan, Northbrook"
+    },
+    "225": {
+        "Region": "LA",
+        "TimezoneOffset": "-6",
+        "Description": "Louisiana: Baton Rouge, New Roads, Donaldsonville, Albany, Gonzales, Greensburg, Plaquemine, Vacherie"
+    },
+    "226": {
+        "Region": "ON",
+        "TimezoneOffset": "-5",
+        "Description": "Canada: South Western Ontario: Windsor"
+    },
+    "228": {
+        "Region": "MS",
+        "TimezoneOffset": "-6",
+        "Description": "Southern Mississippi including coastal areas, Biloxi, Gulfport"
+    },
+    "229": {
+        "Region": "GA",
+        "TimezoneOffset": "-5",
+        "Description": "South Western Georgia: Albany"
+    },
+    "231": {
+        "Region": "MI",
+        "TimezoneOffset": "-5",
+        "Description": "Western Michigan: Northwestern portion of lower Peninsula; Traverse City, Muskegon, Cheboygan, Alanson"
+    },
+    "234": {
+        "Region": "OH",
+        "TimezoneOffset": "-5",
+        "Description": "North Eastern Ohio: Canton, Akron"
+    },
+    "236": {
+        "Region": "BC",
+        "TimezoneOffset": "1.142857143",
+        "Description": "Canada: British Columbia"
+    },
+    "239": {
+        "Region": "FL",
+        "TimezoneOffset": "-5",
+        "Description": "Florida including Lee, Collier, and Monroe Counties.  Excludes the Keys"
+    },
+    "240": {
+        "Region": "MD",
+        "TimezoneOffset": "-5",
+        "Description": "Western Maryland: Silver Spring, Frederick, Gaithersburg"
+    },
+    "242": {
+        "Region": "None",
+        "TimezoneOffset": "-5",
+        "Description": "Bahamas"
+    },
+    "246": {
+        "Region": "None",
+        "TimezoneOffset": "-4",
+        "Description": "Barbados"
+    },
+    "248": {
+        "Region": "MI",
+        "TimezoneOffset": "-5",
+        "Description": "Michigan: Oakland County, Pontiac"
+    },
+    "250": {
+        "Region": "BC",
+        "TimezoneOffset": "1.142857143",
+        "Description": "Canada: British Columbia"
+    },
+    "251": {
+        "Region": "AL",
+        "TimezoneOffset": "-6",
+        "Description": "S Alabama: Mobile and coastal areas, Jackson, Evergreen, Monroeville"
+    },
+    "252": {
+        "Region": "NC",
+        "TimezoneOffset": "-5",
+        "Description": "Eastern North Carolina including Rocky Mount"
+    },
+    "253": {
+        "Region": "WA",
+        "TimezoneOffset": "-8",
+        "Description": "Washington: South Tier - Tacoma, Federal Way"
+    },
+    "254": {
+        "Region": "TX",
+        "TimezoneOffset": "-6",
+        "Description": "Central Texas: Waco, Stephenville"
+    },
+    "256": {
+        "Region": "AL",
+        "TimezoneOffset": "-6",
+        "Description": "Eastern and Northern Alabama including Huntsville, Florence, Gadsden"
+    },
+    "260": {
+        "Region": "IN",
+        "TimezoneOffset": "-5",
+        "Description": "North Eastern Indiana: Fort Wayne"
+    },
+    "262": {
+        "Region": "WI",
+        "TimezoneOffset": "-6",
+        "Description": "South Eastern Wisconsin: including counties of Kenosha, Ozaukee, Racine, Walworth, Washington, Waukesha"
+    },
+    "264": {
+        "Region": "None",
+        "TimezoneOffset": "-4",
+        "Description": "Anguilla"
+    },
+    "267": {
+        "Region": "PA",
+        "TimezoneOffset": "-5",
+        "Description": "South Eastern Pennsylvania: Philadelphia"
+    },
+    "268": {
+        "Region": "None",
+        "TimezoneOffset": "-4",
+        "Description": "Antigua and Barbuda"
+    },
+    "269": {
+        "Region": "MI",
+        "TimezoneOffset": "-5",
+        "Description": "South Western Michigan: Kalamazoo, Saugatuck, Hastings, Battle Creek, Sturgis to Lake Michigan"
+    },
+    "270": {
+        "Region": "KY",
+        "TimezoneOffset": "-6",
+        "Description": "Western Kentucky: Bowling Green, Paducah"
+    },
+    "272": {
+        "Region": "PA",
+        "TimezoneOffset": "-5",
+        "Description": "North Eastern and North Central Pennsylvania: Wilkes-Barre, Scranton"
+    },
+    "276": {
+        "Region": "VA",
+        "TimezoneOffset": "-5",
+        "Description": "South and South Western Virginia: Bristol, Stuart, Martinsville"
+    },
+    "278": {
+        "Region": "MI",
+        "TimezoneOffset": "-5",
+        "Description": "Michigan"
+    },
+    "281": {
+        "Region": "TX",
+        "TimezoneOffset": "-6",
+        "Description": "Texas: Houston Metro area"
+    },
+    "283": {
+        "Region": "OH",
+        "TimezoneOffset": "-5",
+        "Description": "South Western Ohio: Cincinnati"
+    },
+    "284": {
+        "Region": "None",
+        "TimezoneOffset": "-4",
+        "Description": "British Virgin Island"
+    },
+    "289": {
+        "Region": "ON",
+        "TimezoneOffset": "-5",
+        "Description": "Canada: South Central. Ontario: Greater Toronto Area None Durham, Halton, Hamilton-Wentworth, Niagara, Peel, York, and southern Simcoe County.  Excludes Toronto"
+    },
+    "301": {
+        "Region": "MD",
+        "TimezoneOffset": "-5",
+        "Description": "Western Maryland: Silver Spring, Frederick, Camp Springs, Prince George's County"
+    },
+    "302": {
+        "Region": "DE",
+        "TimezoneOffset": "-5",
+        "Description": "Delaware"
+    },
+    "303": {
+        "Region": "CO",
+        "TimezoneOffset": "-7",
+        "Description": "Central Colorado: Denver"
+    },
+    "304": {
+        "Region": "WV",
+        "TimezoneOffset": "-5",
+        "Description": "West Virginia"
+    },
+    "305": {
+        "Region": "FL",
+        "TimezoneOffset": "-5",
+        "Description": "SE Florida: Miami, the Keys"
+    },
+    "306": {
+        "Region": "SK",
+        "TimezoneOffset": "-6/-7*",
+        "Description": "Canada: Saskatchewan"
+    },
+    "307": {
+        "Region": "WY",
+        "TimezoneOffset": "-7",
+        "Description": "Wyoming"
+    },
+    "308": {
+        "Region": "NE",
+        "TimezoneOffset": "0.857142857",
+        "Description": "Western Nebraska: North Platte"
+    },
+    "309": {
+        "Region": "IL",
+        "TimezoneOffset": "-6",
+        "Description": "West Central Illinois: Peoria"
+    },
+    "310": {
+        "Region": "CA",
+        "TimezoneOffset": "-8",
+        "Description": "Southern California: Beverly Hills, West Hollywood, West Los Angeles"
+    },
+    "311": {
+        "Region": "None",
+        "TimezoneOffset": "None",
+        "Description": "Reserved for special applications"
+    },
+    "312": {
+        "Region": "IL",
+        "TimezoneOffset": "-6",
+        "Description": "Illinois: Chicago, downtown only"
+    },
+    "313": {
+        "Region": "MI",
+        "TimezoneOffset": "-5",
+        "Description": "Michigan: Detroit and suburbs"
+    },
+    "314": {
+        "Region": "MO",
+        "TimezoneOffset": "-6",
+        "Description": "South Eastern Missouri: St Louis city and parts of the metro area only"
+    },
+    "315": {
+        "Region": "NY",
+        "TimezoneOffset": "-5",
+        "Description": "N Cent. New York: Syracuse"
+    },
+    "316": {
+        "Region": "KS",
+        "TimezoneOffset": "-6",
+        "Description": "S Kansas: Wichita"
+    },
+    "317": {
+        "Region": "IN",
+        "TimezoneOffset": "-5",
+        "Description": "Central Indiana: Indianapolis"
+    },
+    "318": {
+        "Region": "LA",
+        "TimezoneOffset": "-6",
+        "Description": "North Louisiana: Shreveport, Ruston, Monroe, Alexandria"
+    },
+    "319": {
+        "Region": "IA",
+        "TimezoneOffset": "-6",
+        "Description": "Eastern Iowa: Cedar Rapids"
+    },
+    "320": {
+        "Region": "MN",
+        "TimezoneOffset": "-6",
+        "Description": "Central Minnesota: Saint Cloud including rural Minnesota.  Excludes St. Paul/Minneapolis"
+    },
+    "321": {
+        "Region": "FL",
+        "TimezoneOffset": "-5",
+        "Description": "Florida: Brevard County, Cape Canaveral area; Metro Orlando"
+    }
+};
---
+++ samples/BMS/src/index.js
@@ -0,0 +1,133 @@
+/**
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+ http://aws.amazon.com/apache2.0/
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask my BMS about 9 7 0 7"
+ *  Alexa: "(reads back the description)"
+ */
+
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill'),
+    areaCodes = require('./data');
+
+var APP_ID = 'amzn1.echo-sdk-ams.app.7838e90e-4fbd-4048-93e3-c6b8ec4fce43'; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+/**
+ * BMS CostCenter is a child of AlexaSkill AreaCodeHelper.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var AreaCodeHelper = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+AreaCodeHelper.prototype = Object.create(AlexaSkill.prototype);
+AreaCodeHelper.prototype.constructor = AreaCodeHelper;
+
+AreaCodeHelper.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    var speechText = {
+            speech:  "<speak>Welcome to cost center lookup by Anita Macaluso. You can ask a question like, "
+            +"what is <say-as interpret-as='digits'>9800</say-as> ? ... "
+            +"Now, what cost center would you like?</speak>",
+            type:   AlexaSkill.speechOutputType.SSML
+        },
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+        repromptText = {
+            speech: "For instructions on what you can say, please say help me.",
+            type:   AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+    response.ask(speechText, repromptText);
+};
+
+AreaCodeHelper.prototype.intentHandlers = {
+    "AreaCodeIntent": function (intent, session, response) {
+        var itemSlot = intent.slots.Item,
+            itemName;
+        if (itemSlot && itemSlot.value){
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = "Area Code for " + itemName,
+            location,
+            speechOutput,
+            repromptOutput;
+
+        //Lookup description for a valid area code
+        if (itemName in areaCodes) {
+            location = areaCodes[itemName].Description;
+        }
+
+        if (location) {
+            var speechText = "<speak>The location for " + "<say-as interpret-as='digits'>" + itemName + "</say-as> is " + location + "</speak>";
+            speechOutput = {
+                speech: speechText,
+                type: AlexaSkill.speechOutputType.SSML
+            };
+            response.tellWithCard(speechOutput, cardTitle, location);
+        } else {
+            var speech;
+            if (itemName) {
+                if (itemName === "?") {
+                    speech = "<speak>I'm sorry, I did not understand you. Please say ... 'what is' ... plus a four digit area code.</speak>";
+                } else {
+                    speech = "<speak>I'm sorry.  I could not find cost code " + "<say-as interpret-as='digits'>" + itemName + "</say-as>"
+                        + ". Please say ... 'what is' ... plus a four digit area code.</speak>";
+                }
+            } else {
+                speech = "<speak>I'm sorry, I could not find that cost center.  Please say ... 'what is' ... plus a four digit area code.</speak>";
+            }
+            speechOutput = {
+                speech: speech,
+                type: AlexaSkill.speechOutputType.SSML
+            };
+            repromptOutput = {
+                speech: "Please say ... 'what is' ... plus a four digit area code, or say cancel,  to quit.",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.ask(speechOutput, repromptOutput);
+        }
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = {
+                speech:  "<speak>You get the location of an area code by saying, "
+                +"where is <say-as interpret-as='digits'>8600</say-as> ? ... "
+                +"Now, what area code would you like?</speak>",
+                type:   AlexaSkill.speechOutputType.SSML
+            },
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+            repromptText = {
+                speech: "For instructions on what you can say, please say help me.",
+                type:   AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+        response.ask(speechText, repromptText);
+    }
+};
+
+exports.handler = function (event, context) {
+    var areaCodeHelper = new AreaCodeHelper();
+    areaCodeHelper.execute(event, context);
+};
